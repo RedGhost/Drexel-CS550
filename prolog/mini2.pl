a4ee24889c29034ec17c98cb1b8567514e517ecd
Tim <tim@TIM-LAPTOP.(none)>
2013-06-05 16:04:21 -0400

    Updates for assign, if, while. Need to fix the positive while case.

--- prolog/mini2.pl
+++ prolog/mini2.pl
@@ -8,6 +8,13 @@
 %    V = config(20,[value(x,2),value(y,1)]) ? 
 %
 % reduce_program(config([assign(x,3),assign(y,4)],[]),Env).
+% reduce_program(config([assign(x,plus(1,2))],[]),Env).
+%
+% reduce_program(config([if(3,[assign(x,3)],[assign(x,4)])],[]),Env).
+% reduce_program(config([if(minus(0,4),[assign(x,3)],[assign(x,4)])],[]),Env).
+%
+% reduce_program(config([while(-3,[assign(x,3)])],[value(x,1)]),Env).
+% reduce_program(config([while(x,[assign(x,-1)])],[value(x,1)]),Env).
 
 lookup([value(I,V)|_],I,V).
 lookup([_|Es],I,V) :- lookup(Es,I,V), !.
@@ -44,12 +51,22 @@ valueDelete(X,[value(X,V)|T],R) :- valueDelete(X,T,R).
 valueDelete(X,[value(H,V)|T],[value(H,V)|R]) :- valueDelete(X,T,R).
 
 reduce_stmt(config(assign(I,V),Env),Env1) :- atom(I), integer(V), valueDelete(I,Env,X), append([value(I,V)],X,Env1).
-reduce_stmt(config(assign(I,E),Env),config(assign(I,V),Env)) :- reduce_value(config(E,Env),V).
+reduce_stmt(config(assign(I,E),Env),Env1) :- reduce_value(config(E,Env),V), reduce_stmt(config(assign(I,V),Env),Env1).
+
+reduce_stmt(config(if(V,L1,L2),Env),Env1) :- integer(V), V = 0, reduce_program(config(L2,Env),Env1).
+reduce_stmt(config(if(V,L1,L2),Env),Env1) :- integer(V), V < 0, reduce_program(config(L2,Env),Env1).
+reduce_stmt(config(if(V,L1,L2),Env),Env1) :- integer(V), V > 0, reduce_program(config(L1,Env),Env1).
+reduce_stmt(config(if(E,L1,L2),Env),Env1) :- reduce_value(config(E,Env),V), reduce_stmt(config(if(V,L1,L2),Env),Env1).
+
+reduce_stmt(config(while(V,L),Env),Env1) :- integer(V), V = 0, append(Env,[],Env1).
+reduce_stmt(config(while(V,L),Env),Env1) :- integer(V), V < 0, append(Env,[],Env1).
+reduce_stmt(config(while(V,L),Env),Env1) :- integer(V), V > 0, reduce_program(config(L,Env),Env1). %TODO check the loop value again.
+reduce_stmt(config(while(E,L),Env),Env1) :- reduce_value(config(E,Env),V), reduce_stmt(config(while(V,L),Env),Env1).
 
 %Statements will be passed into this function as a list. e.g. [assign(x,3),assign(y,4)]
 %When the first statement is complete, the resulting environment is passed along as the environment for the next statement.
 reduce_stmt_all(config([],Env),Env).
-reduce_stmt_all(config([E|Es],Env),Env2) :- reduce_stmt(config(E,Env),Env1), reduce_stmt_all(config(Es,Env1),Env2).
+reduce_stmt_all(config([S|Ss],Env),Env2) :- reduce_stmt(config(S,Env),Env1), reduce_stmt_all(config(Ss,Env1),Env2).
 
 reduce_program(config(StmtList,StaringEnv),Env) :- reduce_stmt_all(config(StmtList,StaringEnv),Env).
 %%%
